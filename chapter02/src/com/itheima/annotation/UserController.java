520fdb40666b8adac310ba05eee6f17517b8e0ff
cqfrank0115 <497655640@qq.com>
2019-03-25 10:55:34 +0800

    Add files via upload

--- chapter02/src/com/itheima/annotation/UserController.java
+++ chapter02/src/com/itheima/annotation/UserController.java
@@ -1,10 +1,12 @@
 package com.itheima.annotation;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 
 @Controller("userController")
 public class UserController {
-	@Resource(name="userService");
+	@Resource(name="userService")
 	private UserService userService;
 	public void save() {
 		this.userService.save();
--- chapter02/src/com/itheima/annotation/UserServiceImpl.java
+++ chapter02/src/com/itheima/annotation/UserServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service("userService")
 public class UserServiceImpl implements UserService{
-	@Resource(name="userDao");
+	@Resource(name="userDao")
 	private UserDao uerDao;
 	public void save() {
 		this.uerDao.save();
