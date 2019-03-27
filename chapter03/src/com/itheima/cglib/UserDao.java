6e51f1794110e541132f905d3dd2bc307ff325ad
cqfrank0115 <497655640@qq.com>
2019-03-27 11:22:56 +0800

    Add files via upload

---
+++ chapter03/src/com/itheima/cglib/UserDao.java
@@ -0,0 +1,10 @@
+package com.itheima.cglib;
+
+public class UserDao {
+	public void addUser() {
+		System.out.println("addUser");
+	}
+	public void deleteUser() {
+		System.out.println("deleteUser");
+	}
+}
---
+++ chapter03/src/com/itheima/jdk/UserDaoImpl.java
@@ -0,0 +1,13 @@
+package com.itheima.jdk;
+
+public class UserDaoImpl implements UserDao{
+
+	public void addUser() {
+		System.out.println("addUsers");
+		
+	}
+
+	public void deleteUser() {
+		System.out.println("deleteUsers");
+	}
+}
